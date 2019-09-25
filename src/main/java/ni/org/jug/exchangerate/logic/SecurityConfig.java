f0ad9d461a51e2b92dfb243404f27d26379edac4
Armando Alaniz <arjoalaniz@gmail.com>
2019-09-25 16:45:20 -0600

    Restringir el acceso al recurso cookies

--- src/main/java/ni/org/jug/exchangerate/logic/SecurityConfig.java
+++ src/main/java/ni/org/jug/exchangerate/logic/SecurityConfig.java
@@ -1,10 +1,17 @@
 package ni.org.jug.exchangerate.logic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  *
@@ -14,19 +21,40 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String ENV_USER_NAME = "APP_USER";
+    private static final String ENV_USER_PASSWORD = "APP_PASSWORD";
+    private static final String DEFAULT_CREDENTIALS = "jugni";
 
+    @Autowired
+    Environment environment;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String user = environment.getProperty(ENV_USER_NAME, DEFAULT_CREDENTIALS);
+        String password = environment.getProperty(ENV_USER_PASSWORD, DEFAULT_CREDENTIALS);
+
         auth.inMemoryAuthentication()
-                .withUser("jugni").password("jugni").roles("ADMIN");
+                .withUser(user).password(passwordEncoder.encode(password)).roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/api/banks/*/cookies/*").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/banks/*/cookies").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/api/banks/*/cookies/*").authenticated()
+                .antMatchers(HttpMethod.PUT, "/api/banks/*/cookies/").authenticated()
                 .anyRequest().permitAll()
-                .and().httpBasic();
+                .and().httpBasic()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
