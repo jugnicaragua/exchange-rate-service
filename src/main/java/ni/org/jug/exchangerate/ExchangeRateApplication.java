b7869ceb8a1a7c97362d9620c6015c916f8d94e4
Armando Alaniz <arjoalaniz@gmail.com>
2019-10-19 09:41:57 -0600

    Enviar correo electronico de manera asincrona

--- src/main/java/ni/org/jug/exchangerate/ExchangeRateApplication.java
+++ src/main/java/ni/org/jug/exchangerate/ExchangeRateApplication.java
@@ -9,12 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories("ni.org.jug.exchangerate.repository")
 @EntityScan("ni.org.jug.exchangerate.model")
 @EnableScheduling
+@EnableAsync
 public class ExchangeRateApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ExchangeRateApplication.class);
